c94699337b357a51bdeb30c08ebd9c5fd189c355
Mario Konja <mkonja@teamliquid.com>
2021-07-20 22:29:46 -0400

    update about me

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,70 +1,106 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class About extends Component {
-  render() {
-    return (
-      <div>
-        <section className="colorlib-about" data-section="about">
-        <div className="colorlib-narrow-content">
-            <div className="row">
-            <div className="col-md-12">
-                <div className="row row-bottom-padded-sm animate-box" data-animate-effect="fadeInLeft">
-                <div className="col-md-12">
-                    <div className="about-desc">
-                    <span className="heading-meta">About Us</span>
-                    <h2 className="colorlib-heading">Who Am I?</h2>
-                    <p>I am a computer grad student from DAIICT-Gandhinagar with ICT as my major.I love exploring new technologies and often amazed by the progress we as a human species have mad so far in the recent years (apart from the headphone-jack part!!)</p>
-                    <p>I have started reflecting my ideas and thougths through the medium of words recently so spelling and grammer mistaks are very often.You can write me back if you spot any and don't want to live anymore :P </p>
+    render() {
+        return (
+            <div>
+                <section className="colorlib-about" data-section="about">
+                    <div className="colorlib-narrow-content">
+                        <div className="row">
+                            <div className="col-md-12">
+                                <div
+                                    className="row row-bottom-padded-sm animate-box"
+                                    data-animate-effect="fadeInLeft"
+                                >
+                                    <div className="col-md-12">
+                                        <div className="about-desc">
+                                            <span className="heading-meta">
+                                                About Us
+                                            </span>
+                                            <h2 className="colorlib-heading">
+                                                Who Am I?
+                                            </h2>
+                                            <p>
+                                                I am a Software Engineer grad
+                                                with an insane passion for two
+                                                things: Soccer and Video Games.
+                                                Specifically Liverpool FC and
+                                                anything esports and Twitch.
+                                            </p>
+                                            <p>
+                                                I have always wanted to work in
+                                                the video game industry in
+                                                anyway since I was a child. My
+                                                mom even remembers a day when I
+                                                was in kindergarden and I said
+                                                "I want to make video games!"
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                </div>
-                </div>
-            </div>
-            </div>
-        </div>
-        </section>
-        <section className="colorlib-about">
-        <div className="colorlib-narrow-content">
-            <div className="row">
-            <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
-                <span className="heading-meta">What I do?</span>
-                <h2 className="colorlib-heading">Here are some of my expertise</h2>
-            </div>
-            </div>
-            <div className="row row-pt-md">
-            <div className="col-md-4 text-center animate-box">
-                <div className="services color-1">
-                <span className="icon">
-                    <i className="icon-bulb" />
-                </span>
-                <div className="desc">
-                    <h3>Front-End Web Development</h3>
-                    <p>I have experience building websites and services using React, HTML, CSS, Vue.js, and Angular.js</p>
-                </div>
-                </div>
-            </div>
-            <div className="col-md-4 text-center animate-box">
-                <div className="services color-3">
-                <span className="icon">
-                    <i className="icon-phone3" />
-                </span>
-                <div className="desc">
-                    <h3>Back-End Web Development</h3>
-                    <p>Work in a variety of differnet backends using Spring-Boot, Express.js, and even C#</p>
-                </div>
-                </div>
-            </div>
-            <div className="col-md-4 text-center animate-box">
-                <div className="services color-5">
-                <span className="icon">
-                    <i className="icon-data" />
-                </span>
-                <div className="desc">
-                    <h3>Game Development</h3>
-                    <p>For fun I like to make games and mess around with Unity and Unreal</p>
-                </div>
-                </div>
-            </div>
-            {/*
+                </section>
+                <section className="colorlib-about">
+                    <div className="colorlib-narrow-content">
+                        <div className="row">
+                            <div
+                                className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box"
+                                data-animate-effect="fadeInLeft"
+                            >
+                                <span className="heading-meta">What I do?</span>
+                                <h2 className="colorlib-heading">
+                                    Here are some of my expertise
+                                </h2>
+                            </div>
+                        </div>
+                        <div className="row row-pt-md">
+                            <div className="col-md-4 text-center animate-box">
+                                <div className="services color-1">
+                                    <span className="icon">
+                                        <i className="icon-bulb" />
+                                    </span>
+                                    <div className="desc">
+                                        <h3>Front-End Web Development</h3>
+                                        <p>
+                                            I have experience building websites
+                                            and services using React, HTML, CSS,
+                                            Vue.js, and Angular.js
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-md-4 text-center animate-box">
+                                <div className="services color-3">
+                                    <span className="icon">
+                                        <i className="icon-phone3" />
+                                    </span>
+                                    <div className="desc">
+                                        <h3>Back-End Web Development</h3>
+                                        <p>
+                                            Work in a variety of differnet
+                                            backends using Spring-Boot,
+                                            Express.js, and even C#
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-md-4 text-center animate-box">
+                                <div className="services color-5">
+                                    <span className="icon">
+                                        <i className="icon-data" />
+                                    </span>
+                                    <div className="desc">
+                                        <h3>Game Development</h3>
+                                        <p>
+                                            For fun I like to make games and
+                                            mess around with Unity and Unreal
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            {/*
             <div className="col-md-4 text-center animate-box">
                 <div className="services color-2">
                 <span className="icon">
@@ -99,10 +135,10 @@ export default class About extends Component {
                 </div>
             </div>
             */}
+                        </div>
+                    </div>
+                </section>
             </div>
-        </div>
-        </section>
-      </div>
-    )
-  }
+        );
+    }
 }
